84bb3acb6f025d531a0d67704dcf80e3398645f6
Nick Gerner <nick.gerner@gmail.com>
2020-11-29 13:01:57 -0800

    add some timing output throughout build_features to keep track of partial progress timing

--- build_features.sh
+++ build_features.sh
@@ -54,10 +54,12 @@ FILE_METADATA=${DATADIR}/metadata.gz
 
 # choose the commits you're going to look at (git log)
 # get all the filestatuses for relevant changes
+date > /dev/stderr
 echo "listing commits on $DEFAULT_BRANCH to ${FILE_FILESTATUS}"
 git log --no-renames -m --first-parent --name-status --topo-order --format='%H%x09%ct%x09%P' $DEFAULT_BRANCH -- | gawk 'BEGIN {OFS="\t"} NR == 1 {next_master=$1;last_master=$1} next_master != "" && $1 == next_master { next_master = $3; last_master=$1} $1 ~ /^[a-f0-9]{40}$/ {last_commit = $1} NF > 0 && $1 !~ /^[a-f0-9]{40}$/ { print last_master, last_commit, $0}' | pv > ${FILE_FILESTATUS}
 
 #exclude external files and documentation
+date > /dev/stderr
 echo "filtering external/3rd party files and documentation..."
 if [ -z "$FILE_EXCLUDE_PATHS" ]; then
     cat ${FILE_FILESTATUS} | cut -f 4 | LC_ALL=C sort -u > ${FILE_FILELIST}
@@ -67,6 +69,7 @@ else
         gawk 'BEGIN {n=0} FNR==NR {excluderegex = $0} FNR!=NR { if($1 !~ excluderegex) {print $1}}' <(cat ${FILE_EXCLUDE_PATHS} | tr '\n' '|' | sed 's/|$//') - | grep -v '^$' > ${FILE_FILELIST} || true
 fi
 
+date > /dev/stderr
 echo "checking if any files remain..."
 if [ ! -s "${FILE_FILELIST}" ]; then
     echo "after dropping excluded files, nothing left to analyze"
@@ -76,12 +79,14 @@ if [ ! -s "${FILE_FILELIST}" ]; then
 fi
 
 #get some metadata
+date > /dev/stderr
 echo "getting commit dates..."
 git log --no-renames --first-parent --topo-order --format='%H%x09%ct%x09%P%x09%t%x09%s' $DEFAULT_BRANCH -- | cut -f 1,2 | LC_ALL=C sort -u > ${FILE_COMMIT_DATES}
 
 # create the list of commits
 #I'm explicitly filtering out some files that have irritating characters
 #TODO: make this robust to irritating characters in file names (e.g. spaces)
+date > /dev/stderr
 echo "listing commit files on $DEFAULT_BRANCH to ${FILE_COMMITS}"
 pv ${FILE_FILESTATUS} | ag -v 'actionpack/test/fixtures/public/foo' |\
     gawk -F\\t 'BEGIN {OFS="\t"} FNR==NR {allow[$1]=1} FNR != NR {if($3 ~ /R[0-9][0-9][0-9]/) { if(allow[$4]) {print $1, $2, $3, $4;} if(allow[$5]) {print $1, $2, $3, $5;} } else if(allow[$4]) {print $0} }'\
@@ -95,6 +100,7 @@ if [ ! -s ${FILE_COMMITS} ]; then
     exit 1
 fi
 
+date > /dev/stderr
 echo "files: $(cat ${FILE_FILELIST} | wc -l)" > /dev/stderr
 echo "commits: $(cat ${FILE_COMMITS} | wc -l)" > /dev/stderr
 
@@ -104,6 +110,7 @@ echo "commits: $(cat ${FILE_COMMITS} | wc -l)" > /dev/stderr
 # pull out diffs (git show)
 #create diffs file
 #gawk -F\\t '{printf("echo \"merging %s %s %s\"\n", $1, $2, $4);printf("git show -m --first-parent -U0 --oneline %s -- '"'"'%s'"'"'\n", $2, $4)}' |\
+date > /dev/stderr
 echo "get diffs from those commits (this might take a while) to ${FILE_LINES}"
 cat ${FILE_COMMITS} |\
     gawk -F\\t 'function printgitshow(ncommits, commits, filename) {if(ncommits <= 0) { return; } quoted_filename=filename; if(quoted_filename !~ /^".*"$/) { quoted_filename=sprintf("\"%s\"", quoted_filename);} escaped_filename=filename; gsub(/[\\]/, "\\\\", escaped_filename); gsub(/"/, "\\\"", escaped_filename); printf("git show --no-renames -m --first-parent -U0 --format=\"merging %%H %%H %s%%x0A%%h %%s\" %s -- %s\n", escaped_filename, commits, quoted_filename);} BEGIN {commits="";ncommits=0} ncommits>=100 || $4 != lastf {printgitshow(ncommits, commits, lastf);ncommits=0; commits=""} {ncommits+=1; commits = commits " " $2; lastf=$4} END {printgitshow(ncommits, commits, lastf) }' |\
@@ -113,6 +120,7 @@ cat ${FILE_COMMITS} |\
 # follow line changes through from birth to death (command using process_diffs.awk, this outputs a log of birth/death events for lines, but also dumps the lines of code and new/old diff hunks for all commits)
 #process diffs file (creates linelog with birth/death events,
 #but also codefile, addfile, remfile which has code and new/old diff hunks
+date > /dev/stderr
 echo "follow line lifetime (like git blame. this might take a while) to ${FILE_LINELOG}"
 echo "note, you might see a warning about invalid multibyte data, you can safely ignore that."
 pv ${FILE_LINES} | zcat | gawk -v binariesfile=${FILE_BINARIESFILE} -v codefile=${FILE_CODEFILE} -v addfile=${FILE_ADDFILE} -v remfile=${FILE_REMFILE} -f ${DIR}/process_diffs.awk | gzip -c > ${FILE_LINELOG}
@@ -121,6 +129,7 @@ echo "" >> ${FILE_BINARIESFILE}
 
 #create some relevant metadata
 #adding dates
+date > /dev/stderr
 echo "creating metadata about line lifetime to ${FILE_METADATA}"
 pv ${FILE_LINELOG} | zcat |\
     gawk -F\\t 'BEGIN{OFS="\t"} FNR == 1 { fileindex+=1 } fileindex == 1 { commits[$1] = $2 } fileindex == 2 { binaries[$1] = 1 } fileindex==3 && commits[$3] == "" { print "birth commit not found on line", FNR > "/dev/stderr"; exit 1 } fileindex == 3 && commits[$6] == "" { print "latest commit not found on line", FNR > "/dev/stderr"; exit 1} fileindex == 3 && !binaries[$2] {print commits[$3], commits[$6], $0}' ${FILE_COMMIT_DATES} ${FILE_BINARIESFILE} - |\
